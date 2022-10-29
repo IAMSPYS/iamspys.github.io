80564106077793cd2a4b2fbdb1bddbecbaa40682
IAMSPYS <77228351+IAMSPYS@users.noreply.github.com>
2022-10-29 22:15:48 +0700

    Update topnav.js

--- topnav.js
+++ topnav.js
@@ -1,15 +1,5 @@
 // Topnav html converted to js
 
-document.write("<head>");
-document.write("    <link rel=\"stylesheet\" href=\"style.css\">");
-document.write("    <link rel=\"preconnect\" href=\"https:\/\/fonts.googleapis.com\">");
-document.write("    <link rel=\"preconnect\" href=\"https:\/\/fonts.gstatic.com\" crossorigin>");
-document.write("    <link href=\"https:\/\/fonts.googleapis.com\/css2?family=Poppins:wght@300&display=swap\" rel=\"stylesheet\">");
-document.write("");
-document.write("    <script type=\"text\/javascript\" src=\"script.js\"><\/script>");
-document.write("");
-document.write("<\/head>");
-document.write("");
 document.write("<div class=\"topnav\">");
 document.write("    <a href=\"https:\/\/discord.gg\/JP6b2KEhdy\">Discord<\/a>");
 document.write("    <a href=\"https:\/\/www.reddit.com\/user\/IAMSPYS\">Reddit<\/a>");
