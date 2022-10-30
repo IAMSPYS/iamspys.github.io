4447e17b053348dba02ad9d7fc6252e27a35e60d
IAMSPYS <77228351+IAMSPYS@users.noreply.github.com>
2022-10-30 17:26:25 +0700

    Update topnav.js

--- topnav.js
+++ topnav.js
@@ -5,7 +5,7 @@ document.write("    <a href=\"https:\/\/discord.gg\/JP6b2KEhdy\">Discord<\/a>");
 document.write("    <a href=\"https:\/\/www.reddit.com\/user\/IAMSPYS\">Reddit<\/a>");
 document.write("    <a href=\"https:\/\/www.youtube.com\/c\/SecretStuff-SodaSC\">YouTube<\/a>");
 document.write("    <a href=\"https:\/\/twitter.com\/iamspys_ok\">Twitter<\/a>");
-document.write("    <a href=\"SirapolGames\">Sirapol Games<\/a>");
+document.write("    <a href=\"SirapolGames\">SirapolGames<\/a>");
 document.write("    <a href=\"AboutMe\">About Me<\/a>");
 document.write("    <a href=\"index\">Home<\/a>");
 document.write("<\/div>");
